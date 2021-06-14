06f326e797226c28b29ffda97a32d6d2432927fc
Paul Iverson Cortez <pauliversonc@gmail.com>
2021-06-14 10:00:51 +0800

    datatables in bs4

---
+++ app/Http/Controllers/DatatableController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DatatableController extends Controller
+{
+    public function datatable(){
+        return view('datatable');
+    }
+}
